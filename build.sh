a2b0caa1ae6eb5ac68d598da3897ed50d60ed471
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-09-25 10:41:45 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -1,40 +1,36 @@
 echo 'builindg FRE-NCtools conda package...'
-echo "SRC_DIR / Build directory is:"
-pwd
-echo "Contents of SRC_DIR / Build directory are:"
-ls
+echo "SRC_DIR / Build directory is: $PWD"
+echo -e "Contents of SRC_DIR / Build directory are: \n`ls`"
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
-echo "PRE CONFIGURATION::"
-echo ""
-echo ""
-echo "PATH is:"
-echo $PATH
-echo "LD_LIBRARY_PATH is:"
-echo $LD_LIBRARY_PATH
-echo ""
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PREFIX}/lib
+echo -e "PRE CONFIGURATION::\n\n"
+echo -e "PATH is: \n $PATH"
+#echo -e "LD_LIBRARY_PATH is: \n $LD_LIBRARY_PATH \n"
+
 
 ## this is sufficient
-autoreconf -i
-./configure --prefix=$PREFIX || cat config.log
+mkdir build && cd build
+autoreconf -iv ../
+../configure --prefix=$PREFIX --with-mpi --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
-echo "POST CONFIGURATION::"
-echo ""
-echo ""
-echo "PATH is:"
-echo $PATH
-echo "LD_LIBRARY_PATH is:"
-echo $LD_LIBRARY_PATH
-echo ""
+#export LD_LIBRARY_PATH=${PREFIX}/lib
+echo -e "POST CONFIGURATION::\n\n"
+echo -e "PATH is: \n $PATH"
+#echo "LD_LIBRARY_PATH is:"
+#echo $LD_LIBRARY_PATH
+#echo ""
 
-echo "compiling/building"
-make
-echo "installing into $PREFIX"
+echo "compiling/building and installing into $PREFIX"
 make install
+
+echo "testing"
+make check
+
+echo "removing build directory"
+cd ../ && rm -rf build
 
 ### to test, build-dir option, ala README
 #autoreconf -i
